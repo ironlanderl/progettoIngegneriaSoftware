75c7120d909f0a3275653f565f7e26f4db83cd46
ironlanderl <fabrizio03p@gmail.com>
2025-03-28 15:15:03 +0100

    Implemented Feedback

--- Utenti/Feedback.py
+++ Utenti/Feedback.py
@@ -1,7 +1,33 @@
-import datetime
-
+from datetime import datetime
 
 class Feedback:
-    def __init__(self):
-        self.contenuto: str = ""
-        self.data: datetime.datetime = datetime.datetime(1970,1,1,0,0)
+    def __init__(self, contenuto: str, data: datetime):
+        self._contenuto: str = ""
+        self._data: datetime.datetime = datetime.datetime(1970,1,1,0,0)
+
+    @property
+    def contenuto(self):
+        """The contenuto property."""
+        return self._contenuto
+
+    @contenuto.setter
+    def contenuto(self, value):
+        if value == "":
+            raise ValueException("Il contenuto del feedback non può essere vuoto")
+        self._contenuto = value
+
+    @property
+    def data(self):
+        """The data property."""
+        return self._data
+
+    @data.setter
+    def data(self, value):
+        # Primo controllo: la data deve essere nel formato specificato
+        try:
+            tmp: datetime = datetime.strptime(value, "%Y-%m-%d %H:%M:%S")
+        except:
+            raise ValueError("La data è in un forato errato")
+
+        self._data = tmp
+
